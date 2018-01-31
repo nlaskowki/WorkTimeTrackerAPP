7814c33f1e6def3e005847c2b4182c7b5f592b0e
AxelVanHoyweghen <axel.van.hoyweghen@outlook.com>
2018-01-31 13:24:41 -0500

    Removed DB

--- app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/Sign_In_Screen_Controller.java
+++ app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/Sign_In_Screen_Controller.java
@@ -1,8 +1,5 @@
 package com.worktimetrackerapp.GUI_Interfaces;
 
-/**
- * Created by axelv on 1/30/2018.
- */
 
 public class Sign_In_Screen_Controller {
 
--- app/src/main/java/com/worktimetrackerapp/WTTApplication.java
+++ app/src/main/java/com/worktimetrackerapp/WTTApplication.java
@@ -1,6 +1,14 @@
 package com.worktimetrackerapp;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class WTTApplication{
+public class WTTApplication  extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sidebar_menu);
+
+    }
 }
