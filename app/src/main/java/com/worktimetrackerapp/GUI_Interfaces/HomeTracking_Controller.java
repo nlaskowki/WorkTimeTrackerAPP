f921557230516e08f618ed1261c5a3683a9715cc
Max Dawson <mdawson@oakland.edu>
2018-02-28 14:21:50 -0500

    Update home page for chronometer test

--- app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/HomeTracking_Controller.java
+++ app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/HomeTracking_Controller.java
@@ -3,6 +3,7 @@ package com.worktimetrackerapp.GUI_Interfaces;
 
         import android.app.Fragment;
         import android.os.Bundle;
+        import android.os.SystemClock;
         import android.support.annotation.Nullable;
         import android.view.LayoutInflater;
         import android.view.View;
@@ -14,12 +15,40 @@ package com.worktimetrackerapp.GUI_Interfaces;
 
 public class HomeTracking_Controller extends Fragment {
 
+    private Button TestButton;
+    private Chronometer TestChronometer;
+
+
     View currentView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         currentView = inflater.inflate(R.layout.home_tracking, container, false);
+
+       TestButton = (Button) currentView.findViewById(R.id.jobDoneBtn);
+       TestChronometer = (Chronometer) currentView.findViewById(R.id.TestChrono);
+
+        TestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TestChronometer.setBase(SystemClock.elapsedRealtime());
+                TestChronometer.start();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
         return currentView;
 
 
