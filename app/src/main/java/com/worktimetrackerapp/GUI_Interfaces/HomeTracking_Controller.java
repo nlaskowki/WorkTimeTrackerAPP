af9cec52eccfc164c721f646cfa73b9017781dc2
Max Dawson <mdawson@oakland.edu>
2018-03-03 22:50:10 -0600

    Added some home screen tracking function. Functioning toggle buttons, timer, and wage incrementing based on timer in seconds.

--- app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/HomeTracking_Controller.java
+++ app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/HomeTracking_Controller.java
@@ -1,6 +1,7 @@
 package com.worktimetrackerapp.GUI_Interfaces;
 
 
+        import android.app.Activity;
         import android.app.Fragment;
         import android.os.Bundle;
         import android.os.SystemClock;
@@ -10,13 +11,27 @@ package com.worktimetrackerapp.GUI_Interfaces;
         import android.view.ViewGroup;
         import android.widget.Button;
         import android.widget.Chronometer;
+        import android.widget.CompoundButton;
+        import android.widget.TextView;
+        import android.widget.ToggleButton;
+
 
         import com.worktimetrackerapp.R;
 
 public class HomeTracking_Controller extends Fragment {
 
     private Button TestButton;
-    private Chronometer TestChronometer;
+    private Chronometer workChronometer;
+    private ToggleButton ClockInOutToggleBtn;
+    private ToggleButton TakeBreakToggleBtn;
+    private TextView WageTextView;
+    private double count123;
+    private double wagePerHour = .50;
+    Thread t;
+    boolean continueThread = true;
+    private double totalWageEarned;
+
+    private long holdLastPause;
 
 
     View currentView;
@@ -26,30 +41,124 @@ public class HomeTracking_Controller extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         currentView = inflater.inflate(R.layout.home_tracking, container, false);
 
-        TestButton = (Button) currentView.findViewById(R.id.jobDoneBtn);
-        TestChronometer = (Chronometer) currentView.findViewById(R.id.TestChrono);
+        //TestButton = (Button) currentView.findViewById(R.id.jobDoneBtn);
+        ClockInOutToggleBtn = (ToggleButton) currentView.findViewById(R.id.clockIn_OutToggleBtn);
+        workChronometer = (Chronometer) currentView.findViewById(R.id.workChrono);
+        TakeBreakToggleBtn = (ToggleButton) currentView.findViewById((R.id.breakToggleBtn));
+        WageTextView = (TextView) currentView.findViewById(R.id.wageEarnedTxtVw);
 
-        TestButton.setOnClickListener(new View.OnClickListener() {
+
+        ClockInOutToggleBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 
-                TestChronometer.setBase(SystemClock.elapsedRealtime());
-                TestChronometer.start();
+                if (b){
+
+                    continueThread = true;
+                    count123 = 0;
+                    workChronometer.setBase(SystemClock.elapsedRealtime());
+                    workChronometer.start();
+                    wageTrack();
+
+
+
+                }
+
+                else {
+
+                    workChronometer.stop();
+                    continueThread = false;
+
+                    //This is were wage info gets sent to DB
+                    totalWageEarned = 0;
+
+                }
+
             }
         });
 
+        TakeBreakToggleBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (b){
+
+                    holdLastPause = SystemClock.elapsedRealtime();
+                    workChronometer.stop();
+                    continueThread = false;
+
+
+                }
+
+                else {
+
+                    workChronometer.setBase(workChronometer.getBase() + SystemClock.elapsedRealtime() - holdLastPause);
+                    workChronometer.start();
+                    continueThread = true;
+                    wageTrack();
 
 
 
-
-
-
-
-
-
-
+                }
+            }
+        });
 
         return currentView;
+
+    }
+
+    public void wageTrack(){
+
+        t = new Thread(){
+            @Override
+            public void run(){
+                while (continueThread){
+
+
+                    try {
+                        Thread.sleep(1000);
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                if (totalWageEarned != 0){
+
+                                    double count123 = totalWageEarned;
+
+
+
+                                }
+
+                                count123 += wagePerHour;
+
+                                totalWageEarned += count123;
+
+                                WageTextView.setText("$ " + String.valueOf(count123));
+
+                            }
+                        });
+
+                    }
+
+                    catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+        };
+
+        t.start();
+
+
+
+
+
+
+
+
 
 
 
