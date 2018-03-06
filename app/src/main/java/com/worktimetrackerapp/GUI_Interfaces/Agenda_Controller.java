b4a44d7ed831138892e8a5a33c7ea0e224a2d341
Noah Laskowski <nlaskowski@oakland.edu>
2018-03-06 18:18:27 -0500

    Added calendar
    calendar decoration to mark dates that have activities on them - pending

--- app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/Agenda_Controller.java
+++ app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/Agenda_Controller.java
@@ -3,11 +3,21 @@ package com.worktimetrackerapp.GUI_Interfaces;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import com.worktimetrackerapp.R;
+
+import java.util.Calendar;
+
 
 public class Agenda_Controller extends Fragment {
 
@@ -17,6 +27,27 @@ public class Agenda_Controller extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         currentView = inflater.inflate(R.layout.agenda, container, false);
+
+
+        MaterialCalendarView materialCalendarView = (MaterialCalendarView) currentView.findViewById(R.id.calendarView);
+
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.MONDAY)
+                .setMinimumDate(CalendarDay.from(1900, 1, 1))
+                .setMaximumDate(CalendarDay.from(2100, 12, 31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Toast.makeText(getActivity(), "" + date, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
         return currentView;
     }
 }
