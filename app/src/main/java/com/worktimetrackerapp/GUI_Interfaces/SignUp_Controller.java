dad7df9d7af0d175ebaf2abdf5a5595d0a27db29
Tyler <tastreu@oakland.edu>
2018-03-07 13:29:29 -0500

    updated user info page

--- app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/SignUp_Controller.java
+++ app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/SignUp_Controller.java
@@ -4,18 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-import com.worktimetrackerapp.DB;
-import com.worktimetrackerapp.R;
 import com.worktimetrackerapp.MainActivity;
+import com.worktimetrackerapp.R;
 
 public class SignUp_Controller extends AppCompatActivity {
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up_screen);
+        setContentView(R.layout.activity_user_information);
+        radioGroup = findViewById(R.id.rgroup);
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(SignUp_Controller.this, parent.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         final Button btnSignUp = findViewById(R.id.btn_sign_up);
 
@@ -29,12 +49,25 @@ public class SignUp_Controller extends AppCompatActivity {
 
 
                 //temporary redirection
-                    Intent LaunchHome = new Intent(getApplicationContext(), MainActivity.class);
-                    LaunchHome.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(LaunchHome);
+                Intent LaunchHome = new Intent(getApplicationContext(), MainActivity.class);
+                LaunchHome.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(LaunchHome);
+
+                int rbid = radioGroup.getCheckedRadioButtonId();
+                radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(rbid);
+
+                Toast.makeText(getBaseContext(), radioButton.getText(), Toast.LENGTH_LONG).show();
             }
         });
     }
+
+    // public void rbclick (View v){
+    //     int rbid = radioGroup.getCheckedRadioButtonId();
+    //     radioButton = findViewById(rbid);
+
+    //    Toast.makeText(getBaseContext(), radioButton.getText(), Toast.LENGTH_LONG).show();
+    //}
 
     @Override
     protected void onDestroy() {
