9e23d48419292b82efb6e2098f45bb5e64639c81
Noah Laskowski <nlaskowski@oakland.edu>
2018-03-07 23:32:36 -0500

    removed decorator code because it was disabling the dates not coloring them

--- app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/Agenda_Controller.java
+++ app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/Agenda_Controller.java
@@ -67,8 +67,6 @@ public class Agenda_Controller extends Fragment {
 
         MaterialCalendarView materialCalendarView = (MaterialCalendarView) currentView.findViewById(R.id.calendarView);
 
-        // Add a decorator to disable prime numbered days
-        materialCalendarView.addDecorator(new PrimeDayDisableDecorator());
 
         Calendar calendar = Calendar.getInstance();
         materialCalendarView.setSelectedDate(calendar.getTime());
@@ -105,60 +103,6 @@ public class Agenda_Controller extends Fragment {
 
         return currentView;
     }
-
-    private static class PrimeDayDisableDecorator implements DayViewDecorator {
-
-        @Override
-        public boolean shouldDecorate(CalendarDay day) {
-            return PRIME_TABLE[day.getDay()];
-        }
-
-        @Override
-        public void decorate(DayViewFacade view) {
-            view.setDaysDisabled(true);
-        }
-
-        private static boolean[] PRIME_TABLE = {
-                false,  // 0?
-                false,
-                true, // 2
-                true, // 3
-                false,
-                true, // 5
-                false,
-                true, // 7
-                false,
-                false,
-                false,
-                true, // 11
-                false,
-                true, // 13
-                false,
-                false,
-                false,
-                true, // 17
-                false,
-                true, // 19
-                false,
-                false,
-                false,
-                true, // 23
-                false,
-                false,
-                false,
-                false,
-                false,
-                true, // 29
-                false,
-                true, // 31
-                false,
-                false,
-                false, //PADDING
-        };
-    }
-
-
-
 
     protected void startShowList() throws Exception {
         DB app = (DB) getActivity().getApplication();
