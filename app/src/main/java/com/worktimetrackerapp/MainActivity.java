817b43aff94143d398bddd720634d9bba66b6a84
Tyler <tastreu@oakland.edu>
2018-03-10 21:02:48 -0500

    user info page and sign up controller updated with working code

--- app/src/main/java/com/worktimetrackerapp/MainActivity.java
+++ app/src/main/java/com/worktimetrackerapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.worktimetrackerapp;
 
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
