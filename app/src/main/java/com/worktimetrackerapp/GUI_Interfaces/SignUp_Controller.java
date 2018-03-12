8d8991ef7c863b8a2c4b65b1f349f182d6fdecae
Tyler <tastreu@oakland.edu>
2018-03-12 15:04:26 -0400

    user info page and sign up controller updated with working code

--- app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/SignUp_Controller.java
+++ app/src/main/java/com/worktimetrackerapp/GUI_Interfaces/SignUp_Controller.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -34,7 +35,12 @@ public class SignUp_Controller extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_information);
         Spinner spinner = findViewById(R.id.spinner);
-
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
         company = findViewById(R.id.txt_company);
         jobTitle = findViewById(R.id.txt_job_title);
         employer= findViewById(R.id.txt_employer);
